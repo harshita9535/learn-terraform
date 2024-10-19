20cd7ef0e2670b1c8d8143b7146f2bdc093dddbc
Harshita <harshitabhadimane02051998@gmail.com>
2024-10-19 23:08:23 +0530

    Module demo

--- 05-module/demo/ec2.tf
+++ 05-module/demo/ec2.tf
@@ -2,3 +2,9 @@ resource "aws_instance" "test" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t2.micro"
 }
+
+tags = {
+  Name = var.Name
+}
+
+variable "Name" {}
--- 05-module/demo/test.tf
+++ 05-module/demo/test.tf
@@ -1,9 +1,1 @@
-terraform {
-  required_providers {
-    null = {
-      source  = "hashicorp/null"
-      version = "3.2.2"
-    }
-  }
-}
 resource "null_resource" "test" {}
