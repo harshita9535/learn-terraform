5600d40836c6064138610bddda5bfc756bce7ddd
Harshita <harshitabhadimane02051998@gmail.com>
2024-10-19 13:18:09 +0530

    Vars demo

--- 03-variables/vars.tf
+++ 03-variables/vars.tf
@@ -7,7 +7,16 @@ variable "x" {
 #Declare variable without value
 #variable "y" {}
 
+variable "y_list" {
+  default = [1,2,3]
+}
+
+
 # Print Variable
 output "x" {
   value = var.x
+}
+
+output "y" {
+  value = var.y_list
 }
