fb3fbf27ffba8281569b88302e04297f934dd6ac
Harshita <harshitabhadimane02051998@gmail.com>
2024-10-20 13:01:08 +0530

    Loops.

--- 07-loops/loop.tf
+++ 07-loops/loop.tf
@@ -3,3 +3,18 @@
 resource "null_resource" "test" {
   count = 10
 }
+
+resource "null_resource" "test1" {
+  for_each = var.color-codes
+}
+
+variable "color-codes" {
+  default = {
+    red = 31
+    green = 32
+    yellow = 33
+    blue = 34
+    magenta = 35
+    cyan = 36
+  }
+}
