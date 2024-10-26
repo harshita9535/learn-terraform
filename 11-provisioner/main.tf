e9a749c170d45b67bfa80da8199ec7e9efcaba68
Harshita <harshitabhadimane02051998@gmail.com>
2024-10-26 13:05:21 +0530

    Create pipelines for terraform expense project

--- 11-provisioner/main.tf
+++ 11-provisioner/main.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    null = {
-      source  = "hashicorp/null"
-      version = "3.2.2"
-    }
-  }
-}
 resource "aws_instance" "test" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t2.micro"
